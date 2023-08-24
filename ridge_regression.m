508e10a5e34f13bffe4ab39ed0a5fe644bf30feb
YADengUU <yaqi.deng@igp.uu.se>
2023-08-24 22:34:10 +0800

    fix logical error at input checks

--- ridge_regression.m
+++ ridge_regression.m
@@ -1,40 +1,56 @@
 %================================================
-% Ridge regression with 10-fold cross validation
+% Initiation of ridge regression
 % Input:
 %   X: the Nxp input matrix with N observations on p coefficients
 %   y: the N*1 observation matrix
-%   lambdas: the list of lambdas for regularization
-%   opts: (Optional) 1x1 struct including solver options
-%         standardize: to standardize the input X or not, enter 0 or 1,
-%           default is 0
-%         train_ratio: proportion of data used for training, default 0.8
+%   lambdas: the list of lambdas for regularization, e.g. exp(-8:6)
+%   train_ratio: proportion of data used for training, default 0.8
+%
 %  OUTPUT:
 %   b: coefficient estimates
 %   LRT_result: results from likelihood-ratio test
-%       includes the i-th coefficient, reject null hypothesis or not, p-value
-%   cv_results: the mse from cv of each lambda
+%       includes the i-th coefficient (except the constant term), 
+%                   reject null hypothesis or not, 
+%                   p-value
+%                   test statistic value (chi-squared test)
+%   mse: the mse of the model tested on the test set
+%   r_sq: r-square, goodness of fit
 %================================================
-function [b,LRT_result,cv_results,mse,r_sq,best_lambda] = ridge_regression(X,y,lambdas,opts)
-    
-    
-    % set solver options
-    if nargin < 3              
-        error(message('Not enough inputs'));      
+function [b,LRT_result,mse,r_sq] = ridge_regression(X,y,lambdas,train_ratio)
+
+    if nargin < 2              
+        error('MyError:InadequateInput','Not enough inputs');      
     end
 
-    if (nargin<4)||isempty(opts)||~isfield(opts,'standardize')
-        stdz=0;
-    else
-        stdz=1;
+    % non-numerical entries are not supported yet
+    if ~isnumeric(X)||~isnumeric(y)
+        error('MyError:NonnumericalInput','Data entries must be numeric');
+    end
+
+    [nx,p]=size(X);
+    ny=length(y);
+    if nx~=ny
+        error('MyError:UnmatchingSize','Sample sizes of input and output matrix do not match');
     end
     
-    if (nargin<4)||isempty(opts)||~isfield(opts,'train_ratio')
+    if ~exist('lambdas','var')
+        lambdas=exp(-8:6);
+    end
+    if ~isnumeric(lambdas)
+        error('MyError:NonnumericalLambda','Ridge tuning parameter lambda must be numeric');
+    end
+
+    if ~exist('train_ratio','var')
         train_ratio=0.8;
+    end
+    
+    if ~isnumeric(train_ratio)
+        error('MyError:NonnumericalTraining','Training ratio must be numeric');
     else
-        if opts.train_ratio<0.7
-            error(message('Training set should be at least 70% of the whole data'));
-        else
-            train_ratio=opts.train_ratio;
+        if train_ratio<0.7
+            error('MyError:SmallTraining','Training set should be at least 70 percent of the whole data');
+        elseif train_ratio>=1
+            error('MyError:HugeTraining','Training set cannot be greater than 100 percent of the whole data');
         end
     end
     
@@ -44,68 +60,12 @@ function [b,LRT_result,cv_results,mse,r_sq,best_lambda] = ridge_regression(X,y,l
         X = X(~nas_ind,:);
         y = y(~nas_ind);
     end
+    fprintf('Removed %d samples due to missing values\n',sum(nas_ind));
 
-    [N,p]=size(X);
-   
-    % standardize the columns of input matrix X if requested
-    if stdz == 1
-        X = normalize(X);
+    if p<999
+        [b,LRT_result,mse,r_sq] = ridge_nopar(X,y,lambdas,train_ratio);
+    else
+        [b,LRT_result,mse,r_sq] = ridge_parallel(X,y,lambdas,train_ratio);
     end
-
-
-    % append a column of ones to X, make it a design matrix
-    X = [ones(N,1),X];
-
-    % split data into training and test set
-    partition = cvpartition(N,'HoldOut',1-train_ratio);
-    train_inds = training(partition);
-    X_train = X(train_inds,:);
-    y_train = y(train_inds);
-    test_inds = test(partition);
-    X_test = X(test_inds,:);
-    y_test = y(test_inds);
-
-    % compute cross-validation of the lambdas
-    cv_results = zeros(length(lambdas),2);
-
-    for i = 1:length(lambdas)
-        lambda = lambdas(i);
-        [cv_err]= cross_validation(X_train,y_train,lambda);
-        cv_results(i,:)=[lambda,cv_err];
-    end
-
-    % choose the lambda that gives smallest cross-validation error
-    [err_min,ind_min] = min(cv_results(:,2));
-    best_lambda = cv_results(ind_min,1);
-
-    % fit the model with all training data with this lambda
-    I = eye(p+1);
-    b = (transpose(X_train)*X_train+best_lambda*I)\transpose(X_train)*y_train;
-
-    % prediction using the resulting model on test set
-    y_pred = X_test*b;
-    residuals = y_pred-y_test;
-    mse = mean(residuals.^2);
-    
-    % goodness-of-fit
-    r_sq=1-(sum(residuals.^2)/sum((y_test-mean(y_test)).^2));
-
-    % p-value using LRT
-    %  log-likelihood of full model
-    logL_full = compute_loglikelihood(y_pred,y_test);
-
-    % fit reduced model
-    LRT_result = zeros(p,4);
-    for i = 2:(p+1)
-        X_rm_train = X_train(:,[1:i-1,i+1:end]);
-        X_rm_test = X_test(:,[1:i-1,i+1:end]);
-        I = eye(p);
-        b_rm = (transpose(X_rm_train)*X_rm_train+best_lambda*I)\transpose(X_rm_train)*y_train;
-        y_rm_pred=X_rm_test*b_rm;
-        [h,pval,stat] = lratiotest(logL_full,compute_loglikelihood(y_rm_pred,y_test),1);
-        LRT_result(i-1,:)=[i-1,h,pval,stat];
-    end
-    
 
 end
-
